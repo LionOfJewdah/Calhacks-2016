4aa0350cd79c66d4d63cbfcf994aeefb8fa19784
Hemant Kunda <hemantkunda@gmail.com>
2016-11-13 01:45:00 -0800

    checking sql queyr

--- DjangoScripts/api-test_price_fun.py
+++ DjangoScripts/api-test_price_fun.py
@@ -156,8 +156,10 @@ for jvar in range (0, 10) :
 			day = priceDate[1]
 			year = priceDate[2]
 			try:
-				cursor.execute("insert into endofday (date, close, open, high, low, lastsale, volume) values (\'%s-%s-%s 00:00:00\', %s, %s, %s, %s, %s, %s)" 
-										% (year, month, day, price['Close'], price['Open'], price['High'], price['Low'], price['LastSale'], price['Volume']))
+				endOfDaySql = "insert into endofday (date, close, open, high, low, lastsale, volume) values (\'%s-%s-%s 00:00:00\', %s, %s, %s, %s, %s, %s)" 
+										% (year, month, day, price['Close'], price['Open'], price['High'], price['Low'], price['LastSale'], price['Volume'])
+				print endOfDaySql
+				cursor.execute(endOfDaySql)
 			except(Exception) as e:
 				print("Error %d with executing SQL: %s" % (e.args[0], e.args[1]))
 			try:
@@ -180,8 +182,10 @@ for jvar in range (0, 10) :
 		day = priceDate[1]
 		year = priceDate[2]
 		try:
-			cursor.execute("insert into summaryinfo (date, percent_change, prices, symbol) values (\'%s-%s-%s 00:00:00\', %s, %s, \'%s\')" % 
-				(year, month, day, closing_price['PercentChange'][i], closing_price['Prices'][i], closing_price['Symbol']))
+			summarySql = "insert into summaryinfo (date, percent_change, prices, symbol) values (\'%s-%s-%s 00:00:00\', %s, %s, \'%s\')" % 
+				(year, month, day, closing_price['PercentChange'][i], closing_price['Prices'][i], closing_price['Symbol'])
+			print summarySql
+			cursor.execute(summaryinfo)
 		except(Exception) as e:
 				print("Error %d with executing SQL: %s" % (e.args[0], e.args[1]))
 	stockPredix.commit()
